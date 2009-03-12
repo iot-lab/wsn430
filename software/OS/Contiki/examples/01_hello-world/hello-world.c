a1d63468b5d0b7f6be90d9a8f74c3b55da25a3ef
burindes <burindes@e59e8366-ff4a-0410-815a-bc13a6274fd4>
2009-03-12 16:57:43 +0000

    * reorganized examples * 3 examples described on sentools.info * started to adapt and test rime examples
    git-svn-id: svn+ssh://scm.gforge.inria.fr/svn/senstools/trunk@170 e59e8366-ff4a-0410-815a-bc13a6274fd4

---
+++ software/OS/Contiki/examples/01_hello-world/hello-world.c
@@ -0,0 +1,78 @@
+/**
+ * \file
+ *         A very simple Contiki application showing to independent processes running
+ * \author
+ *         Clément Burin des Roziers <clement.burin-des-roziers@inria.fr>
+ */
+
+#include "contiki.h"
+#include "dev/leds.h"
+
+#include <stdio.h> /* For printf() */
+/*---------------------------------------------------------------------------*/
+/* We declare the two processes */
+PROCESS(hello_world_process, "Hello world process");
+PROCESS(blink_process, "LED blink process");
+
+/* We require the processes to be started automatically */
+AUTOSTART_PROCESSES(&hello_world_process, &blink_process);
+/*---------------------------------------------------------------------------*/
+/* Implementation of the first process */
+PROCESS_THREAD(hello_world_process, ev, data)
+{
+    // variables are declared static to ensure their values are kept
+    // between kernel calls.
+    static struct etimer timer;
+    static int count = 0;
+    
+    // any process mustt start with this.
+    PROCESS_BEGIN();
+    
+    // set the etimer module to generate an event in one second.
+    etimer_set(&timer, CLOCK_CONF_SECOND);
+    while (1)
+    {
+        // wait here for an event to happen
+        PROCESS_WAIT_EVENT();
+
+        // if the event is the timer event as expected...
+        if(ev == PROCESS_EVENT_TIMER)
+        {
+            // do the process work
+            printf("Hello, world #%i\n", count);
+            count ++;
+            
+            // reset the timer so it will generate an other event
+            // the exact same time after it expired (periodicity guaranteed)
+            etimer_reset(&timer);
+        }
+        
+        // and loop
+    }
+    // any process must end with this, even if it is never reached.
+    PROCESS_END();
+}
+/*---------------------------------------------------------------------------*/
+/* Implementation of the second process */
+PROCESS_THREAD(blink_process, ev, data)
+{
+    static struct etimer timer;
+    static uint8_t leds_state = 0;
+    PROCESS_BEGIN();
+    
+    while (1)
+    {
+        // we set the timer from here every time
+        etimer_set(&timer, CLOCK_CONF_SECOND / 4);
+        
+        // and wait until the vent we receive is the one we're waiting for
+        PROCESS_WAIT_EVENT_UNTIL(ev == PROCESS_EVENT_TIMER);
+        
+        // update the LEDs
+        leds_off(0xFF);
+        leds_on(leds_state);
+        leds_state += 1;
+    }
+    PROCESS_END();
+}
+/*---------------------------------------------------------------------------*/
---
+++ software/OS/Contiki/examples/02_event-post/event-post.c
@@ -0,0 +1,111 @@
+/**
+ * \file
+ *         Event posting between processes example application.
+ * \author
+ *         Clément Burin des Roziers <clement.burin-des-roziers@inria.fr>
+ */
+
+#include "contiki.h"
+#include "dev/leds.h"
+
+#include <stdio.h> /* For printf() */
+
+/* Driver Include */
+#include "ds1722.h"
+
+/* Variables: the application specific event value */
+static process_event_t event_data_ready;
+
+/*---------------------------------------------------------------------------*/
+/* We declare the two processes */
+PROCESS(temp_process, "Temperature process");
+PROCESS(print_process, "Print process");
+
+/* We require the processes to be started automatically */
+AUTOSTART_PROCESSES(&temp_process, &print_process);
+/*---------------------------------------------------------------------------*/
+/* Implementation of the first process */
+PROCESS_THREAD(temp_process, ev, data)
+{
+    // variables are declared static to ensure their values are kept
+    // between kernel calls.
+    static struct etimer timer;
+    static int count = 0;
+    static int average, valid_measure;
+    
+    // those 3 variables are recomputed at every run, therefore it is not
+    // necessary to declare them static.
+    int measure;
+    uint8_t msb, lsb;
+    
+    // any process mustt start with this.
+    PROCESS_BEGIN();
+    
+    /* allocate the required event */
+    event_data_ready = process_alloc_event();
+    
+    /* Initialize the temperature sensor */
+    ds1722_init();
+    ds1722_set_res(10);
+    ds1722_sample_cont();
+    
+    average = 0;
+    
+    // set the etimer module to generate an event in one second.
+    etimer_set(&timer, CLOCK_CONF_SECOND/4);
+    
+    while (1)
+    {
+        // wait here for the timer to expire
+        PROCESS_WAIT_EVENT_UNTIL(ev == PROCESS_EVENT_TIMER);
+        
+        leds_toggle(LEDS_BLUE);
+        
+        // do the process work
+        msb = ds1722_read_MSB();
+        lsb = ds1722_read_LSB();
+        
+        measure = ((uint16_t)msb) << 2;
+        measure += (lsb >> 6) & 0x03;
+        
+        average += measure;
+        count ++;
+        
+        if (count == 4)
+        {
+            // average the sum and store
+            valid_measure = average >> 2;
+            
+            // reset variables
+            average = 0;
+            count = 0;
+            
+            // post an event to the print process
+            // and pass a pointer to the last measure as data
+            process_post(&print_process, event_data_ready, &valid_measure);
+        }
+        
+        // reset the timer so it will generate another event
+        etimer_reset(&timer);
+    }
+    // any process must end with this, even if it is never reached.
+    PROCESS_END();
+}
+/*---------------------------------------------------------------------------*/
+/* Implementation of the second process */
+PROCESS_THREAD(print_process, ev, data)
+{
+    PROCESS_BEGIN();
+    
+    while (1)
+    {
+        // wait until we get a data_ready event
+        PROCESS_WAIT_EVENT_UNTIL(ev == event_data_ready);
+        
+        // display it
+        printf("temperature = %u.%u\n", (*(int*)data)>>2, ((*(int*)data)&0x3)*25);
+        
+    }
+    PROCESS_END();
+}
+/*---------------------------------------------------------------------------*/
