619581f2cf7f062854aeaa9386a76125b41538e1
burindes <burindes@e59e8366-ff4a-0410-815a-bc13a6274fd4>
2009-03-17 14:15:59 +0000

    * fixed a cc1100 contiki implementation bug, preventing from sending big packet. 128 bytes length packets are now supported. * minor modif to UDP echo server example
    git-svn-id: svn+ssh://scm.gforge.inria.fr/svn/senstools/trunk@173 e59e8366-ff4a-0410-815a-bc13a6274fd4

--- software/OS/Contiki/examples/04_echo-udp/echo-server.c
+++ software/OS/Contiki/examples/04_echo-udp/echo-server.c
@@ -29,7 +29,7 @@ static void udphandler(process_event_t ev, process_data_t data)
         /* Set the last byte of the received data as 0 in order to print it. */
         int len = uip_datalen();
         ((char *)uip_appdata)[len] = 0;
-        printf("Receiver received from %u.%u.%u.%u:%u: '%s'\n", uip_ipaddr_to_quad(&UDP_HDR->srcipaddr), HTONS(UDP_HDR->srcport), (char*)uip_appdata);
+        printf("Received from %u.%u.%u.%u:%u: '%s'\n", uip_ipaddr_to_quad(&UDP_HDR->srcipaddr), HTONS(UDP_HDR->srcport), (char*)uip_appdata);
         
         /* Prepare the response datagram in a local buffer */
         memcpy(udpdata, "rx=", 3);
@@ -61,24 +61,18 @@ AUTOSTART_PROCESSES(&example_udp_server_process);
  */
 PROCESS_THREAD(example_udp_server_process, ev, data)
 {
-
-    uip_ipaddr_t ipaddr;
-
+    
     PROCESS_BEGIN();
     printf("UDP Echo Server test\n");
     
-    /* Set address to all zeroes */
-    uip_ipaddr(&ipaddr, 0,0,0,0);
-    
     /* Create a UDP 'connection' with IP 0.0.0.0 and port 0 as remote host.
      * This means the stack will accepts UDP datagrams from any node. */
-    udpconn = udp_new(&ipaddr, HTONS(0), NULL);
+    udpconn = udp_new(NULL, HTONS(0), NULL);
     
     /* Bind the UDP 'connection' to the port 50000. That's the port we're listening on. */
     udp_bind(udpconn, HTONS(50000));
 
-    printf("Created connection with remote peer %d.%d.%d.%d\n", uip_ipaddr_to_quad(&ipaddr));
-    printf("local/remote port %u/%u\n", HTONS(udpconn->lport),HTONS(udpconn->rport));
+    printf("listening on UDP port %u\n", HTONS(udpconn->lport));
     
     while(1) {
         /* Wait until we have an event caused by tcpip interaction */
