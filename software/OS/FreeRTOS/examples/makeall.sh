afa198ffc5410e0d9c87d52d6f03885c87fb577a
burindes <burindes@e59e8366-ff4a-0410-815a-bc13a6274fd4>
2010-07-05 08:37:40 +0000

    * updated ADC example with TDMA com. * added ADC sink example with TDMA, displays measures
    git-svn-id: svn+ssh://scm.gforge.inria.fr/svn/senstools/trunk@444 e59e8366-ff4a-0410-815a-bc13a6274fd4

--- software/OS/FreeRTOS/examples/makeall.sh
+++ software/OS/FreeRTOS/examples/makeall.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 
-SUBDIRS="01_simple_multitask 02_queue_multitask 03_semphr_synchro 04_mutex_multitask 05_network_device 06_coordinator 07_node_appli 08_sink_appli"
+SUBDIRS="01_simple_multitask 02_queue_multitask 03_semphr_synchro 04_mutex_multitask 05_network_device 06_coordinator 07_node_appli 08_sink_appli 09_adc 10_adc_sink"
 
 echo $(date) > compile.log
 
@@ -18,6 +18,13 @@ do
 		echo " ERROR" >> ../compile.log
 		ok=0
 	fi
+	cd - > /dev/null
+done
+
+for i in $SUBDIRS
+do
+	cd $i
+	make clean
 	cd - > /dev/null
 done
 
