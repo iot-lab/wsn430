cfe606b40941b782dc2546ee68674a674fe7146c
harter <harter@e59e8366-ff4a-0410-815a-bc13a6274fd4>
2011-10-04 15:59:09 +0000

    Config file missing, it prevented the example to work
    git-svn-id: svn+ssh://scm.gforge.inria.fr/svn/senstools/trunk@591 e59e8366-ff4a-0410-815a-bc13a6274fd4

---
+++ software/OS/FreeRTOS/examples/06_tdma_sink_adc/tdma_userconfig.h
@@ -0,0 +1,17 @@
+/*
+ * tdma_userconfig.h
+ *
+ *  Created on: Aug 24, 2010
+ *      Author: burindes
+ */
+
+#ifndef TDMA_USERCONFIG_H_
+#define TDMA_USERCONFIG_H_
+
+#define SLOT_COUNT 12
+#define RADIO_CHANNEL 10
+#define SLOT_TIME_MS 15
+
+
+
+#endif /* TDMA_USERCONFIG_H_ */
