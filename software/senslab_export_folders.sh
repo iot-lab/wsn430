775f703fbcb3b08ca74cc139e80f1d2b21dd592d
Gaëtan Harter <hartergaetan@gmail.com>
2012-05-16 14:46:06 +0200

    Adding a script which generates the export variable to put in bashrc

---
+++ software/senslab_export_folders.sh
@@ -0,0 +1,23 @@
+#! /bin/sh
+
+# Generate export variables for .bashrc.
+# It exports all the variable required to compile programs for senslab
+
+
+
+#  FIT_ECO=$(git rev-parse --show-toplevel)
+# --show-toplevel is not supported on git versions from senslab VMs :/
+FIT_ECO=$(dirname $(git rev-parse --git-dir))
+
+cat << EOF
+
+# add the following lines to your ~/.bashrc file
+
+# Senslab variables
+export FIT_ECO=${FIT_ECO}
+export WSN430_DRIVERS_PATH=\${FIT_ECO}/software/drivers/wsn430
+export WSN430_LIB_PATH=\${FIT_ECO}/software/lib
+export FREERTOS_PATH=\${FIT_ECO}/software/OS/FreeRTOS
+export CONTIKI_PATH=\${FIT_ECO}/software/OS/Contiki
+EOF
+
