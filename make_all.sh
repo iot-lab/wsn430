6d1a5f53aa95eec689c3edb32e0f90ca347bf443
Gaëtan Harter <gaetan.harter@inria.fr>
2014-03-27 19:00:54 +0100

    Make Makefile.common include fail if wsn430 not correctly defined.

---
+++ make_all.sh
@@ -0,0 +1,11 @@
+#! /bin/bash
+
+#
+# Try to build all Makefiles
+#    It should be improved to, at the end, print only errors or warnings
+#
+
+for i in $(find . -iname Makefile | grep -v TinyOS); do
+    make -s -C $(dirname $i) clean  --no-print-directory   RADIO=WITH_CC1101
+    make -s -C $(dirname $i) --no-print-directory   RADIO=WITH_CC1101 || { echo $i; echo ; echo ; }
+done
