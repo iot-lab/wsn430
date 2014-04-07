b820dcf31f4985d7538eff8ab4ba000e9c3418e9
Gaëtan Harter <gaetan.harter@inria.fr>
2014-04-07 17:43:53 +0200

    Cleanup after make

--- make_all.sh
+++ make_all.sh
@@ -8,4 +8,5 @@
 for i in $(find . -iname Makefile | grep -v TinyOS); do
     make -s -C $(dirname $i) clean  --no-print-directory   RADIO=WITH_CC1101
     make -s -C $(dirname $i) --no-print-directory   RADIO=WITH_CC1101 || { echo $i; echo ; echo ; }
+    make -s -C $(dirname $i) clean  --no-print-directory   RADIO=WITH_CC1101
 done
